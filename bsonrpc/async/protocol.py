c41c49ac5d8301f9e58c1cb24b536773b45f4930
Jussi Seppälä <jussi.seppala@ray.fi>
2017-04-06 15:25:44 +0300

    midcommit

--- bsonrpc/async/protocol.py
+++ bsonrpc/async/protocol.py
@@ -4,6 +4,7 @@
 import asyncio
     
 from asyncio import Queue
+from bsonrpc.async.socket_queue import ProtocolQueue
 from bsonrpc.exceptions import BsonRpcError, DecodingError
 from bsonrpc.framing import JSONFramingRFC7464
 from bsonrpc.options import MessageCodec
@@ -11,76 +12,6 @@ from bsonrpc.rpc import RpcBase
 from bsonrpc.socket_queue import BSONCodec, JSONCodec
 
 __license__ = 'http://mozilla.org/MPL/2.0/'
-
-
-class ProtocolQueue(object):
-    '''
-    Duplex queue:
-        - protocol -> data_received -> get
-        - put -> transport.write
-    '''
-
-    def __init__(self, transport, codec):
-        self._is_closed = False
-        self._codec = codec
-        self._transport = transport
-        self._bbuffer = b''
-        self._queue = Queue()  # TODO join in the end
-
-    def _close_transport(self):
-        if self._transport and not self._transport.is_closing():
-            self._transport.close()
-        self._is_closed = True
-
-    def _to_queue(self, bbuffer):
-        b_msg, bbuffer = self._codec.extract_message(bbuffer)
-        while b_msg is not None:
-            try:
-                self._queue.put(self._codec.loads(b_msg))
-            except DecodingError as e:
-                self._queue.put(e)
-            b_msg, bbuffer = self._codec.extract_message(bbuffer)
-        return bbuffer
-
-    def data_received(self, data):
-        try:
-            self._bbuffer = self._to_queue(self._bbuffer + data)
-        except Exception as e:
-            self._queue.put(e)
-            self._close_transport()
-
-    def get(self):
-        return self._queue.get()
-
-    def put(self, item):
-        '''
-        Put item to queue -> codec -> socket.
-
-        :param item: Message object.
-        :type item: dict, list or None
-        '''
-        if self._is_closed:
-            raise BsonRpcError('Attempt to put items to closed queue.')
-        if self._transport:
-            if item is None:
-                self._close_transport()
-            else:
-                msg_bytes = self._codec.into_frame(self._codec.dumps(item))
-                self._transport.write(msg_bytes)
-
-    def connection_lost(self, exc):
-        if exc:
-            self._queue.put(exc)
-        self._queue.put(None)
-        self._is_closed = True
-
-    @property
-    def is_closed(self):
-        return self._is_closed
-
-    def close(self):
-        self._close_transport()
-        # TODO self._queue.join()   maybe
 
 
 class Reference(object):
